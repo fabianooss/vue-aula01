2884e0539d5cbfbb1cff5b0ce0eb618789fb08bb
fabiano <fabiano.oss@gmail.com>
2019-09-09 20:15:22 -0300

    aula

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ nomeProjeto.toUpperCase() }}</h1>        
+
+    <p :title="getMensagem()">Autor: {{ autor }} </p>
+
+    <p>Ano: {{ ano }} </p>
+
+    <p>Data: {{ data.toLocaleString() }}</p>
+
+    <div v-show="isAutorOriginal()">
+      <p>Informações do projeto:</p>
+      <ul>
+        <li>bla</li>
+      </ul>
+    </div>
+
+
+    <button @click="alterarAutor()">Alterar autor</button>
+
+    <button @click="alterarAutor(true)">Alterar para autor original</button>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  // atributos
+  data() {
+    // JSON - JavaScript object notation
+    return {
+      nomeProjeto: 'Meu primeiro projeto',
+      autor: 'Fabiano Oss',
+      ano: 2019,
+      data: new Date()
+    }
+  },
+  methods: {
+    alterarAutor(voltarParaOriginal = false) {
+      if (voltarParaOriginal) {
+        this.autor = 'Fabiano Oss'
+      }
+      else {
+        this.autor = "José da Silva"  
+      }
+      this.data = new Date()
+    },
+    getMensagem() {
+      if (this.autor.startsWith('Fabiano')) {
+        return "Este é o verdadeiro autor"
+      }
+      return "Este autor é fake"
+    },
+    isAutorOriginal() {
+      return this.autor.startsWith('Fabiano')
+    }
   }
+
 }
 </script>
 
@@ -21,7 +66,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
   margin-top: 60px;
 }

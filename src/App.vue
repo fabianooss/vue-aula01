78dbb44d0042c3a810acb55150652665f138cad3
fabiano <fabiano.oss@gmail.com>
2019-09-13 19:55:55 -0300

    app

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,38 @@
 
     <p>Data: {{ data.toLocaleString() }}</p>
 
+    <p>Cidade: {{ cidade }}
+      <button @click="mudarParaMaiuscula()">Mudar para maiúscula</button>
+      <input type="text" v-model="cidade"/>
+    </p>
+
     <div v-show="isAutorOriginal()">
       <p>Informações do projeto:</p>
-      <ul>
-        <li>bla</li>
+
+      <form @submit.prevent="adicionarInfo()">
+        <input type="url" required
+          v-model="urlParaAdicionar"/>
+
+        <button type="submit">Adicionar</button> 
+      </form>
+      <ul v-for="i in info" :key="i">
+        <li>
+           <a :href="i" target="_blank">{{i}}</a>
+        </li>
       </ul>
+
+      <table border=1>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+        </tr>
+        <tr v-for="obj in info2" :key="obj.id">
+          <td>{{obj.id}}</td>
+          <td>{{obj.nome}}</td>
+        </tr>
+      </table>
+
+
     </div>
 
 
@@ -34,10 +61,25 @@ export default {
       nomeProjeto: 'Meu primeiro projeto',
       autor: 'Fabiano Oss',
       ano: 2019,
-      data: new Date()
+      data: new Date(),
+      info: [
+        "http://www.vuejs.org", 
+        "http://www.microsoft.com", 
+        "http://www.google.com"
+      ],
+      info2: [
+        {id: 1, nome:"info1"},
+        {id: 2, nome:"info2"},
+      ],
+      urlParaAdicionar: null,
+
+      cidade: 'Blumenau'
     }
   },
   methods: {
+    mudarParaMaiuscula() {
+      this.cidade = this.cidade.toUpperCase()
+    },
     alterarAutor(voltarParaOriginal = false) {
       if (voltarParaOriginal) {
         this.autor = 'Fabiano Oss'
@@ -55,6 +97,34 @@ export default {
     },
     isAutorOriginal() {
       return this.autor.startsWith('Fabiano')
+    },
+    adicionarInfo() {
+      let encontrou = 
+        this.info.includes(this.urlParaAdicionar)
+
+      this.info.forEach(e => {
+        console.log(e)
+      })  
+
+      let temAlgum = this.info.some(e => {
+        return e == this.urlParaAdicionar
+      })
+      console.log(temAlgum)
+
+      let elemento = this.info.find(e => {
+        return e == this.urlParaAdicionar
+      })
+      console.log(elemento)
+
+
+
+      if (! encontrou) {
+        this.info.push(this.urlParaAdicionar)
+        this.limpaTela()
+      }
+    },
+    limpaTela() {
+      this.urlParaAdicionar = null
     }
   }
 
